3fe3016fd1cd017fb7bd0036cf599aeda8cf6e58
Anjali Khant <anjali@Anjalis-MacBook-Pro.local>
2021-06-01 15:06:38 -0400

    Added request rspec for Users Controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "it returns a success response" do
+      get "/users/"
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET /show" do
+    it 'it returns a success response' do
+      user = User.create(first_name: 'First', last_name: 'Last', email: 'anjali@gmail.com')
+      get '/users/show', params: { id: user.to_param }
+      expect(response).to have_http_status(:success)
+    end
   end
 end
